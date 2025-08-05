e06fc4cfecc5e33a857716956e8553f9dafd338a
sunkaravamsikrishna <vamsikrishna7753@gmail.com>
2025-08-05 16:49:31 +0530

    vamsi

--- src/main/java/Hybrid_Test_Runner.java
+++ src/main/java/Hybrid_Test_Runner.java
@@ -1,7 +1,7 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="/Users/balaseshagiriprasadmunagala/eclipse-workspace/Hybrid_Automation_Framework/Features/LoginFeature.feature",glue="stepdefinitions")
+@CucumberOptions(features="C:\\Users\\vkris\\eclipse-workspace33\\Hybrid_Automation_Framework\\Features\\LoginFeature.feature",glue="stepdefinitions")
 public class Hybrid_Test_Runner extends AbstractTestNGCucumberTests{
 	
 	
