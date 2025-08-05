e34d66116d935e20bf31bd82578165e6d00ccf38
prasadmunagala <prasadmunagala214@gmail.com>
2025-08-05 08:12:10 +0530

    proj_day1

---
+++ src/main/java/Hybrid_Test_Runner.java
@@ -0,0 +1,9 @@
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features="/Users/balaseshagiriprasadmunagala/eclipse-workspace/Hybrid_Automation_Framework/Features/LoginFeature.feature",glue="stepdefinitions")
+public class Hybrid_Test_Runner extends AbstractTestNGCucumberTests{
+	
+	
+
+}
