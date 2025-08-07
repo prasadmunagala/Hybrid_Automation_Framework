a598d5a7d63a3cc8c032ce0f082b01a8f336e6df
chanduevuri <chandu10102000@gmail.com>
2025-08-07 12:47:58 +0530

    message

--- src/main/java/hybrid_test_runner.java
+++ src/main/java/hybrid_test_runner.java
@@ -1,7 +1,7 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="C:\\Users\\chand\\eclipse-workspace\\Hybrid_Automation_Framework\\Hybrid_project\\Byvrid_Automation_Framework",glue="stepdefinitions")
+@CucumberOptions(features="/Users/balaseshagiriprasadmunagala/eclipse-workspace/Hybrid_Automation_Framework/Features/LoginFeature.feature",glue="stepdefinitions")
 public class hybrid_test_runner extends AbstractTestNGCucumberTests{
 	
 	
