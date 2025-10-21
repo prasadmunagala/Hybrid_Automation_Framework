baf4478d4174fcffe2f7d37841d214ea8f396f88
rajuPallam <rajukumar8179950855@gmail.com>
2025-10-21 11:10:47 +0530

    raju

--- src/main/java/feature1/feature1.java
+++ src/main/java/feature1/feature1.java
@@ -4,7 +4,8 @@ public class feature1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+		System.out.println("rajukumar");
+		System.out.println("rajukumar");
 		}
 
 }
